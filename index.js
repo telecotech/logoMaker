b7ebb33d1f6512fee6de3c79f2fd805feccbe967
Mullens <telecotech82@gmail.com>
2023-04-02 15:33:22 -0400

    checking functionality

--- index.js
+++ index.js
@@ -21,24 +21,32 @@ const questions = [
   },
   {
     type: 'input',
-    name: 'shapecolor',
+    name: 'shapeColor',
     message: 'Enter the shape color (keyword or hex):',
   },
 ];
 
-function generateSvg(answers) {
-  const { text, textColor, shape, shapeColor } = answers;
+
   
-  // Create the SVG string template
-  const svgString = `
-    <svg width="100" height="100">
-      <rect width="100" height="100" fill="${shapeColor}" />
-      <text x="50" y="50" fill="${textColor}" text-anchor="middle" font-size="40">${text}</text>
-    </svg>
-  `;
-  
-  return svgString;
-}
+
+    
+    // Create the SVG string template
+    function generateSvg(answers) {
+      const { text, textColor, shape, shapeColor } = answers;
+      
+      // Create the SVG string template
+      const svgString = `
+        <svg width="100" height="100">
+        <shape="${shape}" />
+        < fill="${shapeColor}" />
+          <text x="50" y="50" fill="${textColor}" text-anchor="middle" font-size="40">${text}</text>
+        </svg>
+      `;
+      
+      return svgString;
+    }
+    
+
 
 function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, err => {
@@ -54,7 +62,11 @@ function init() {
   inquirer.prompt(questions)
     .then(answers => {
       const svgString = generateSvg(answers);
-      writeToFile('logo.svg', svgString);
+      if (svgString) {
+        writeToFile('logo.svg', svgString);
+      } else {
+        console.error('Failed to generate SVG string');
+      }
     })
     .catch(error => {
       console.error(error);
